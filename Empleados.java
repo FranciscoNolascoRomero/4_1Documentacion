8f66823816e7cbf364571c1f6fee80265542f36b
FNR <francisconolascoromero@gmail.com>
2023-04-12 12:33:20 +0200

    Version 0.0.5 Añadir enlace

--- Empleados.java
+++ Empleados.java
@@ -6,6 +6,10 @@ package ies;
  * @author Francisco Nolasco
  * @version 0.0
  */
+
+/**
+ *  @see <a href = "http://www.aprenderaprogramar.com" /> aprenderaprogramar.com – Didáctica en programación </a>
+ */
 public class Empleado {
 	private int emp_no;
 	private String nombre;
